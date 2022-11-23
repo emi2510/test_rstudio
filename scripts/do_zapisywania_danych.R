264efca526cc57c1ee0f725249da875ab1e1cedb
emi2510 <s98760@pollub.edu.pl>
2022-11-23 14:16:40 +0100

    Dodałam pliki danych i skrypt do zapisywania danych.

---
+++ scripts/do_zapisywania_danych.R
@@ -0,0 +1,6 @@
+
+library(rio)
+
+export(mtcars, file="data/samochody.csv") //eksportowanie pliku do data
+export(iris, file="data/kwiatki.xlsx")
+export(LifeCycleSavings, file="data/oszczednosci.txt")
